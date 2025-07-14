a7bbc456b3411b60cef725c5572f003abba383ed
Rubiss1118 <emo63703@gmail.com>
2025-07-14 17:24:56 -0600

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://github.com/Rubiss1118/TareaFinal.git',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
